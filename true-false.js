c8ab3a766d332c2799920a9c96cc4caeb6d497b8
Kazi Mashry <kazimashry@gmail.com>
2020-08-07 01:16:58 +0600

    true false

---
+++ true-false.js
@@ -0,0 +1,23 @@
+// falsy:
+// 0
+// ""
+// undefined
+// null
+// NaN(not a number)
+// false
+
+// Truthy
+// '0'
+// ' '
+// []
+// {}
+// "false"
+
+
+const age = null;
+
+if(age) {
+    console.log("Conditiion is true");
+} else {
+    console.log("Condition is false");
+}
